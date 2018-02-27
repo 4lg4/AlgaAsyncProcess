4613fbb06dc5158daffaa45bea17c974dd6a2428
4lg4 <akgleal@gmail.com>
2018-02-27 12:27:03 +1100

    Added initial structure to deploy as a package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-module exports async (promise) => {
+export default async (promise) => {
     try {
         return [null, await promise];
     } catch (err){
         return [err];
     }
-};
+}
---
+++ test/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by www.Alga.me on 27/2/18.
+ */
+
+
+import AlgaAsyncProcess from '../src'
+
+
+Promise
+    .resolve()
+    .resolve()
+
