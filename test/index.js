a186e07601f3ffaa1c08cf77492aec2816e7d959
4lg4 <akgleal@gmail.com>
2018-04-09 11:03:44 +1000

    updated eslint

--- test/index.js
+++ test/index.js
@@ -4,32 +4,31 @@
 
 import assert from 'assert';
 // import AlgaAsyncProcess, {resolve, reject} from '../dist/index';
-import AlgaAsyncProcess, {resolve, reject} from '../src/index';
+import algaAsyncProcess, {resolve, reject} from '../src/index';
 
 describe(`AlgaAsyncProcess https://github.com/4lg4/AlgaAsyncProcess`, ()=> {
-
   describe(`default`, ()=> {
     it(`should return variable as a success`, async () => {
       const expected = 'RIGHT';
-      const [err, success] = await AlgaAsyncProcess(Promise.resolve(expected));
+      const [err, success] = await algaAsyncProcess(Promise.resolve(expected));
       assert.equal(success, expected);
     });
 
     it(`should return variable as an error`, async () => {
       const expected = 'ERROR';
-      const [err, success] = await AlgaAsyncProcess(Promise.reject(expected));
+      const [err, success] = await algaAsyncProcess(Promise.reject(expected));
       assert.equal(err, expected);
     });
 
     it(`should return an array of objects`, async () => {
       const expected = [{id: 1},{id: 2},{id: 3},{id: 4}];
-      const [err, success] = await AlgaAsyncProcess(Promise.resolve(expected));
+      const [err, success] = await algaAsyncProcess(Promise.resolve(expected));
       assert.equal(success, expected);
     });
 
     it(`should return an array resulted from the promise all`, async () => {
       const expected = ['RIGHT', 'RIGHT2', 'RIGHT3'];
-      const [err, a, b, c] = await AlgaAsyncProcess(Promise.all([
+      const [err, a, b, c] = await algaAsyncProcess(Promise.all([
         Promise.resolve(expected[0]),
         Promise.resolve(expected[1]),
         Promise.resolve(expected[2]),
